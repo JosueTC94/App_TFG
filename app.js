89401b01fa9b9800adae8982203f54edd8e85123
JosueTC94 <alu0100763492@ull.edu.es>
2017-02-27 10:27:54 +0000

    Testing

--- app.js
+++ app.js
@@ -4,69 +4,149 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var basePath = process.cwd();
-var estructura = require(path.join(__dirname,'models','bd.js'));
+var passport = require('passport');
+var expressLayouts = require('express-ejs-layouts');
 
+var LocalStrategy = require('passport-local').Strategy;
+
+var estructura = require(path.join(__dirname,'models','bd.js'));
 const Waspmote = estructura.Waspmote;
 const Medida = estructura.Medida;
+const Usuario = estructura.Usuario;
 
 const controlWaspmote = require(path.join(__dirname,'config','waspmoteControl.js'));
-
+const controlUsuario = require(path.join(__dirname,'config','usuarioControl.js'));
 
 //passport-local
+passport.use(new LocalStrategy(
+  function(username, password, cb) {
+    console.log("Identificando usuario:");
+    console.log("User->"+username);
+    console.log("Password->"+password);
+
+    controlUsuario.verifyUser(username, password, (err, user) =>
+    {
+        if(err)
+        {
+          console.log("error");
+          return cb(null,false);
+        }
+
+        if(user)
+        {
+          console.log("Usuario logueado:"+JSON.stringify(user));
+          return cb(null, user);
+        }
+        else
+        {
+          console.log("Usuario no verificado");
+          return cb(null,null);
+        }
+    });
+  }
+));
 
 //sessions
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null,obj);
+});
+
+// Configure view engine to render EJS templates.
+app.use(express.static(path.join(__dirname,'public/')));
+app.set("views", __dirname+'/views');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function (req, res, next) {
-    console.log('Hello World! Waspmote with MAC:'+req.query.MAC);
+    console.log('Hello World!');
     // res.render('index');
 });
+
+app.get('/login',
+  passport.authenticate('local', {failureRedirect: '/error'}),
+  function(req,res) {
+    console.log("Login exitoso");
+    res.render('home');
+});
+
+
 
 app.get('/insertar_medida',function(req,res)
 {
   //Verificar waspmote
 // http://localhost:3000/insertar_medida?Temperatura=21&Humedad=1&Presion=2&ContaminantesAire=3&COLevel=4&CO2Level=5
-  controlWaspmote.siregistrada(req.query.MAC, (err,id)=>
-  {
-    if(err)
-    {
-      console.error(err);
-      //Comunicar al administrador del sistema.
-    }
-    console.log("Registrando medida para Waspmote:"+id);
-    
-    let med = new Medida({
-      Temperatura: req.query.Temperatura,
-      Humedad: req.query.Humedad,
-      Presion: req.query.Presion,
-      ContaminantesAire: req.query.ContaminantesAire,
-      COLevel: req.query.COLevel,
-      CO2Level: req.query.CO2Level,
-      _creator: id
-    });
-    med.save((err)=>
-    {
-      if(err) console.error("Error:"+err);
-      console.log(`Guardada ${med}`);
-    }).then(()=>
-    {
-      Medida
-        .findOne({Temperatura: req.query.Temperatura,
-        Humedad: req.query.Humedad,
-        Presion: req.query.Presion,
-        ContaminantesAire: req.query.ContaminantesAire,
-        COLevel: req.query.COLevel,
-        CO2Level: req.query.CO2Level,
-        Fecha: req.query.Fecha
-        })
-        .populate('_creator')
-        .exec((err,medida)=>
-        {
-          if(err) return console.log(err);
-        }).then(()=>{});
-    });
-  });
+  console.log("Insertando medida...");
+  // controlWaspmote.siregistrada(req.query.MAC, (err,id)=>
+  // {
+  //   if(err)
+  //   {
+  //     console.error(err);
+  //     res.redirect('/');
+  //     //Comunicar al administrador del sistema.
+  //   }
+  //
+  //   console.log("Registrando medida para Waspmote:"+id);
+  //
+  //   if(id != null)
+  //   {
+  //     let med = new Medida({
+  //       Temperatura: req.query.Temperatura,
+  //       Humedad: req.query.Humedad,
+  //       Presion: req.query.Presion,
+  //       ContaminantesAire: req.query.ContaminantesAire,
+  //       COLevel: req.query.COLevel,
+  //       CO2Level: req.query.CO2Level,
+  //       _creator: id
+  //     });
+  //     med.save((err)=>
+  //     {
+  //       if(err) console.error("Error:"+err);
+  //       console.log(`Guardada ${med}`);
+  //     }).then(()=>
+  //     {
+  //       Medida
+  //         .findOne({Temperatura: req.query.Temperatura,
+  //         Humedad: req.query.Humedad,
+  //         Presion: req.query.Presion,
+  //         ContaminantesAire: req.query.ContaminantesAire,
+  //         COLevel: req.query.COLevel,
+  //         CO2Level: req.query.CO2Level,
+  //         Fecha: req.query.Fecha
+  //         })
+  //         .populate('_creator')
+  //         .exec((err,medida)=>
+  //         {
+  //           if(err) return console.log(err);
+  //         }).then(()=>{
+  //             res.redirect('/');
+  //         });
+  //     });
+  //   }
+  //
+  // });
 });
 
-app.listen(3000, function () {
+app.get('/error', (req,res)=>
+{
+    res.render('error');
+});
+
+app.listen(80, function () {
   console.log('Example app listening on port 3000!');
 });
--- models/bd.js
+++ models/bd.js
@@ -1,6 +1,7 @@
 
 "use strict";
 
+var bcrypt = require('bcrypt-nodejs');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/Test');
 
@@ -11,15 +12,45 @@ db.once('open', function(){
 });
 
 const Schema = mongoose.Schema;
+
+const UsuarioSchema = new Schema({
+    Nombre: String,
+    Apellidos: String,
+    Usuario: { type: String, required: true, unique: true },
+    Password: { type: String, required: true , set: password => bcrypt.hashSync(password)},
+    Admin: Boolean,
+    Creado: Date
+}, {collection: "usuarios"});
+
+// UsuarioSchema.methods.verifyUser = ((username, password, cb) =>
+// {
+//   console.log("Accedi");
+//   Usuario.find({Usuario: username}, (err,datos)=>
+//   {
+//     if(err)
+//       return cb(err, null);
+//
+//     if(bcrypt.compareSync(password, datos.Password))
+//     {
+//       return cb(null, datos);
+//     }
+//     else
+//     {
+//       return cb("Password incorrecto", null);
+//     }
+//   });
+// });
+
 const WaspmoteSchema = new Schema({
     Latitud: Number,
     Longitud: Number,
+    Estado: Boolean,
     LevelBattery: {
       type: Number,
       min: 0,
       max: 100
     },
-    MAC: String
+    MAC: { type: String, required: true, unique: true, dropDups: true }
 },{collection:"waspmotes"});
 
 const MedidaSchema = new Schema({
@@ -33,60 +64,89 @@ const MedidaSchema = new Schema({
     _creator: [{type: Schema.Types.ObjectId, ref: "Waspmote"}]
 }, {collection:"medidas"});
 
+const Usuario = mongoose.model('Usuario', UsuarioSchema);
 const Waspmote = mongoose.model('Waspmote', WaspmoteSchema);
 const Medida = mongoose.model('Medida', MedidaSchema);
 
-let wasp1 = new Waspmote(
-  {
-      Latitud: -17.345788,
-      Longitud: -20.463736,
-      LevelBattery: 58,
-      MAC: "MAC1"
-  }
-);
-
-wasp1.save((err)=>
+//Provisional pa testeo
+Usuario.remove({}, (err)=>
 {
-    if(err)
-    {
-      throw err;
-    }
-
-    console.log(`Creado ${wasp1}`);
-    console.log(`Id: ${wasp1._id}`);
-
-    let med1 = new Medida({
-        Temperatura: 22,
-        Humedad: 1111111,
-        Presion: 2222222,
-        ContaminantesAire: 33333333,
-        COLevel: 4444444,
-        CO2Level:55555555,
-        _creator: wasp1._id
+    let admin = new Usuario({
+      Nombre: "Josue",
+      Apellidos: "Toledo Castro",
+      Usuario: "JosueTC94",
+      Password: "Cacahuete",
+      Admin: true,
+      Creado: Date.now()
     });
 
-    med1.save((err)=>
+    admin.save((err) =>
     {
-      if(err) console.log("Error:"+err);
-      console.log(`Creado por ${med1}`);
-    }).then(()=>
+      if(err)
+        throw err;
+
+      console.log(`Creado ${admin}`);
+    });
+});
+
+Waspmote.remove({}, (err)=>
+{
+  Medida.remove({}, (err)=>
+  {
+    let wasp1 = new Waspmote(
     {
-        Medida
-          .findOne({Temperatura: 22,
+        Latitud: -17.345788,
+        Longitud: -20.463736,
+        LevelBattery: 58,
+        Estado: true,
+        MAC: "MAC1"
+    });
+
+    wasp1.save((err)=>
+    {
+      if(err)
+      {
+        throw err;
+      }
+
+      console.log(`Creado ${wasp1}`);
+      console.log(`Id: ${wasp1._id}`);
+
+      let med1 = new Medida({
+          Temperatura: 22,
           Humedad: 1111111,
           Presion: 2222222,
           ContaminantesAire: 33333333,
           COLevel: 4444444,
           CO2Level:55555555,
-          Fecha: new Date('01.02.2012')
-          })
-          .populate('_creator')
-          .exec((err,medida)=>
-          {
-            if(err) return console.log(err);
-          }).then(()=>{});
+          _creator: wasp1._id
+      });
+
+      med1.save((err)=>
+      {
+        if(err) console.log("Error:"+err);
+        console.log(`Creado por ${med1}`);
+      }).then(()=>
+      {
+          Medida
+            .findOexports.Usuario = UsuarioSchemane({Temperatura: 22,
+            Humedad: 1111111,
+            Presion: 2222222,
+            ContaminantesAire: 33333333,
+            COLevel: 4444444,
+            CO2Level:55555555,
+            Fecha: new Date('01.02.2012')
+            })
+            .populate('_creator')
+            .exec((err,medida)=>
+            {
+              if(err) return console.log(err);
+            }).then(()=>{});
+      });
     });
+  });
 });
 
+exports.Usuario = Usuario;
 exports.Waspmote = Waspmote;
 exports.Medida = Medida;
